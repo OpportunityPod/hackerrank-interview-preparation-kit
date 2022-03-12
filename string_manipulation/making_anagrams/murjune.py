c1aada4a802d473a111c42bece3ab182e2cf96bf
murjune <87055456+murjune@users.noreply.github.com>
2022-03-12 22:30:36 +0900

    Create murjune.py

---
+++ string_manipulation/making_anagrams/murjune.py
@@ -0,0 +1,17 @@
+
+# 시간복잡도 : O(N)
+def makeAnagram(a, b):
+    d1 = [0]*26; # 문자열 a을 구성하는 알파벳 개수 담을 테이블
+    d2 = [0]*26; # 문자열 b을 구성하는 알파벳 개수 담을 테이블
+    for c in a:
+        d1[ord(c)-97] += 1; 
+
+    for c in b:
+        d2[ord(c)-97] += 1;
+    
+    ans = 0;
+
+    for i in range(26):
+        ans += abs(d1[i]-d2[i]); # 알파벳의 개수가 일치하지 않으면 그 차를 ans에 더한다
+    
+    return ans;
